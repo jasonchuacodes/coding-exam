8bad4fce3cfe81b3e5a200ce2f8e2f802785efd7
jasonchuacodes <85822633+jasonchuacodes@users.noreply.github.com>
2022-04-01 00:12:07 +0800

    Create CustomInput.vue

---
+++ src/components/CustomInput.vue
@@ -0,0 +1,24 @@
+<template>
+  First Name: 
+	<input type="text" 
+		:value="firstName" 
+		@input="$emit('update:first-name', $event.target.value)"
+		placeholder="Enter first name"
+	>
+	Last Name: 
+	<input type="text" 
+		:value="lastName" 
+		@input="$emit('update:last-name', $event.target.value)"
+		placeholder="Enter last name"
+	>
+	
+</template>
+<script>
+export default {
+	props: {
+		firstName: String,
+		lastName: String
+	},
+	emits: ['update:first-name', 'update:last-name'],
+}
+</script>
